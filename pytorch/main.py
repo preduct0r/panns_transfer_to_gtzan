53b1be55b65bf72c9d282eb7165b100a14d48c2f
den <preductor@gmail.com>
2021-03-12 12:45:14 +0300

    1000

--- pytorch/main.py
+++ pytorch/main.py
@@ -27,7 +27,7 @@ from evaluate import Evaluator
 # для воспроизводимости результатов
 # random.seed(0)
 np.random.seed(0)
-torch.manual_seed(0)
+torch.manual_seed(1000)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 os.environ["PYTHONHASHSEED"] = str(24)
